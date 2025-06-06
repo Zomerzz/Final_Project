8e4ace9e2afe532c3ad87529be4e4a9b1d8aaad0
N9C9 <nicolo.casertano@gmail.com>
2025-06-06 19:20:57 +0200

    implementa l'interfaccia AutoreService e la classe JpaAutoreService

--- src/main/java/generation/italy/org/ravenclaw/models/services/JpaAutoreService.java
+++ src/main/java/generation/italy/org/ravenclaw/models/services/JpaAutoreService.java
@@ -1,7 +1,48 @@
 package generation.italy.org.ravenclaw.models.services;
 
+import generation.italy.org.ravenclaw.models.entities.Autore;
+import generation.italy.org.ravenclaw.models.repositories.AutoreRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class    JpaAutoreService implements AutoreService{
+public class JpaAutoreService implements AutoreService{
+    private AutoreRepository autoreRepo;
+
+    @Override
+    public Optional<Autore> findAutoreById(int id) {
+        return autoreRepo.findById(id);
+    }
+
+    @Override
+    public List<Autore> finAllAutori() {
+        return autoreRepo.findAll();
+    }
+
+    @Override
+    public Autore saveAutore(Autore autore) {
+        return autoreRepo.save(autore);
+    }
+
+    @Override
+    public Autore updateAutore(Autore autore) {
+        Optional<Autore> optAutore = autoreRepo.findById(autore.getAutoreId());
+        if(optAutore.isEmpty()) {
+            throw new EntityNotFoundException();
+        }
+        return saveAutore(autore);
+    }
+
+    @Override
+    public boolean deleteAutore(int id) {
+        Optional<Autore> optAutore = autoreRepo.findById(id);
+        if(optAutore.isEmpty()) {
+            throw new EntityNotFoundException();
+        }
+        autoreRepo.deleteById(id);
+        return true;
+    }
 }
