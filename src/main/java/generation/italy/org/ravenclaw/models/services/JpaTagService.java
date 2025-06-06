28bd959b4b29404a142d50594820b6b928650e94
CamillaMar <camillamarchioro@fadbrera.edu.it>
2025-06-06 16:08:41 +0000

    implementa i metodi CRUD nel service di Tag

--- src/main/java/generation/italy/org/ravenclaw/models/services/JpaTagService.java
+++ src/main/java/generation/italy/org/ravenclaw/models/services/JpaTagService.java
@@ -1,7 +1,54 @@
 package generation.italy.org.ravenclaw.models.services;
 
+import generation.italy.org.ravenclaw.models.entities.Tag;
+import generation.italy.org.ravenclaw.models.repositories.TagRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JpaTagService implements TagService{
+    private TagRepository tagRepo;
+
+    @Autowired
+    public JpaTagService(TagRepository tagRepo){
+        this.tagRepo = tagRepo;
+    }
+
+    @Override
+    public Optional<Tag> findTagById(int id){
+        return tagRepo.findById(id);
+    }
+
+    @Override
+    public List<Tag> findAllTags(){
+        return tagRepo.findAll();
+    }
+
+    @Override
+    public Tag saveTag(Tag tag){
+        return tagRepo.save(tag);
+    }
+
+    @Override
+    public Tag updateTag(Tag tag){
+        Optional<Tag> optTag = tagRepo.findById(tag.getTagId());
+        if(optTag.isEmpty()) {
+            throw new EntityNotFoundException();
+        }
+        return tagRepo.save(tag);
+    }
+
+    @Override
+    public boolean deleteTag(int id){
+        Optional<Tag> optTag = tagRepo.findById(id);
+        if(optTag.isEmpty()) {
+            throw new EntityNotFoundException();
+        }
+        tagRepo.deleteById(id);
+        return true;
+    }
 }
--- src/main/java/generation/italy/org/ravenclaw/models/services/TagService.java
+++ src/main/java/generation/italy/org/ravenclaw/models/services/TagService.java
@@ -1,6 +1,19 @@
 package generation.italy.org.ravenclaw.models.services;
 
+import generation.italy.org.ravenclaw.models.entities.Tag;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TagService {
+    Optional<Tag> findTagById(int id);
+
+    List<Tag> findAllTags();
+
+    Tag saveTag(Tag tag);
+
+    Tag updateTag(Tag tag);
+
+    boolean deleteTag(int id);
 }
