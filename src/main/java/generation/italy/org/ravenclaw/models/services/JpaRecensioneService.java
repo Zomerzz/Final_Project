9bf384e8365a828a6ab653337652dd08f21be236
CamillaMar <camillamarchioro@fadbrera.edu.it>
2025-06-06 17:23:53 +0000

    implementa i metodi CRUD del service di Recensione

--- src/main/java/generation/italy/org/ravenclaw/models/services/JpaRecensioneService.java
+++ src/main/java/generation/italy/org/ravenclaw/models/services/JpaRecensioneService.java
@@ -1,7 +1,54 @@
 package generation.italy.org.ravenclaw.models.services;
 
+import generation.italy.org.ravenclaw.models.entities.Recensione;
+import generation.italy.org.ravenclaw.models.repositories.RecensioneRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JpaRecensioneService implements RecensioneService{
+    private RecensioneRepository recensioneRepo;
+
+    @Autowired
+    public JpaRecensioneService(RecensioneRepository recensioneRepo){
+        this.recensioneRepo = recensioneRepo;
+    }
+
+    @Override
+    public Optional<Recensione> findRecensioneById(int id){
+        return recensioneRepo.findById(id);
+    }
+
+    @Override
+    public List<Recensione> findAllRecensioni(){
+        return recensioneRepo.findAll();
+    }
+
+    @Override
+    public Recensione saveRecensione(Recensione recensione){
+        return recensioneRepo.save(recensione);
+    }
+
+    @Override
+    public Recensione updateRecensione(Recensione recensione){
+        Optional<Recensione> optRecensione = recensioneRepo.findById(recensione.getRecensioneId());
+        if(optRecensione.isEmpty()) {
+            throw new EntityNotFoundException();
+        }
+        return recensioneRepo.save(recensione);
+    }
+
+    @Override
+    public boolean deleteRecensione(int id){
+        Optional<Recensione> optRecensione = recensioneRepo.findById(id);
+        if(optRecensione.isEmpty()) {
+            throw new EntityNotFoundException();
+        }
+        recensioneRepo.deleteById(id);
+        return true;
+    }
 }
--- src/main/java/generation/italy/org/ravenclaw/models/services/RecensioneService.java
+++ src/main/java/generation/italy/org/ravenclaw/models/services/RecensioneService.java
@@ -1,6 +1,20 @@
 package generation.italy.org.ravenclaw.models.services;
 
+import generation.italy.org.ravenclaw.models.entities.Recensione;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RecensioneService {
+
+    Optional<Recensione> findRecensioneById(int id);
+
+    List<Recensione> findAllRecensioni();
+
+    Recensione saveRecensione(Recensione recensione);
+
+    Recensione updateRecensione(Recensione recensione);
+
+    boolean deleteRecensione(int id);
 }
