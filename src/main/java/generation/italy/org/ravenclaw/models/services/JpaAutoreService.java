1d115cc7e20de6d954ba5b53a7d76d6f7fe6e361
Zomerzz <aresufilippo2005@gmail.com>
2025-06-06 18:06:58 +0200

    Aggiunge le interfacce ad angular

--- src/main/java/generation/italy/org/ravenclaw/models/services/JpaAutoreService.java
+++ src/main/java/generation/italy/org/ravenclaw/models/services/JpaAutoreService.java
@@ -3,5 +3,5 @@ package generation.italy.org.ravenclaw.models.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JpaAutoreService implements AutoreService{
+public class    JpaAutoreService implements AutoreService{
 }
