0f1c3019fef7a47847fa586a7582dc1fefc6729b
Colum-nar <edoardobognanni@gmail.com>
2025-06-06 15:52:54 +0100

    aggiunto  cartella del progetto front end

--- src/main/java/generation/italy/org/ravenclaw/controllers/LibroController.java
+++ src/main/java/generation/italy/org/ravenclaw/controllers/LibroController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/libro")
-public class LibroController {
+public class  LibroController {
     private LibroService libroService;
 
     @Autowired
