782f6cd51d9200cd90af52e13d7d6cc7885b22b3
LorenzoCoretti1 <lorenzo.coretti1@gmail.com>
2025-06-07 10:25:40 +0200

    Aggiunge l'utente service

--- src/main/java/generation/italy/org/ravenclaw/models/services/JpaUtenteService.java
+++ src/main/java/generation/italy/org/ravenclaw/models/services/JpaUtenteService.java
@@ -1,7 +1,52 @@
 package generation.italy.org.ravenclaw.models.services;
 
+import generation.italy.org.ravenclaw.models.entities.Autore;
+import generation.italy.org.ravenclaw.models.entities.Libro;
+import generation.italy.org.ravenclaw.models.entities.Utente;
+import generation.italy.org.ravenclaw.models.repositories.AutoreRepository;
+import generation.italy.org.ravenclaw.models.repositories.UtenteRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JpaUtenteService implements UtenteService{
+    private UtenteRepository utenteRepo;
+
+    @Override
+    public Optional<Utente> findUtenteById(int id) {
+        return utenteRepo.findById(id);
+    }
+
+    @Override
+    public List<Utente> findAllUtenti() {
+        return utenteRepo.findAll();
+    }
+
+    @Override
+    public Utente saveUtente(Utente utente) {
+        return utenteRepo.save(utente);
+    }
+
+    @Override
+    public Utente updateUtente(Utente utente) {
+        Optional<Utente> optUtente = utenteRepo.findById(utente.getUtenteId());
+        if(optUtente.isEmpty()){
+            throw new EntityNotFoundException();
+        }
+        return utenteRepo.save(utente);
+    }
+
+    @Override
+    public boolean deleteUtente(int id) {
+        Optional<Utente> optUtente = utenteRepo.findById(id);
+        if(optUtente.isEmpty()){
+            throw new EntityNotFoundException();
+        }
+        utenteRepo.deleteById(id);
+        return true;
+    }
+
 }
--- src/main/java/generation/italy/org/ravenclaw/models/services/UtenteService.java
+++ src/main/java/generation/italy/org/ravenclaw/models/services/UtenteService.java
@@ -1,6 +1,20 @@
 package generation.italy.org.ravenclaw.models.services;
 
+import generation.italy.org.ravenclaw.models.entities.Autore;
+import generation.italy.org.ravenclaw.models.entities.Utente;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UtenteService {
+    Optional<Utente> findUtenteById(int id);
+
+    List<Utente> findAllUtenti(); //mancava una d
+
+    Utente saveUtente(Utente utente);
+
+    Utente updateUtente(Utente utente);
+
+    boolean deleteUtente(int id);
 }
