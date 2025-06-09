a347c8bbd7bb89a21107520b31412266b2cf20aa
elvis_la_fata <elvislafata@gmail.com>
2025-06-09 11:46:50 +0200

    Aggiunte criteriaQuery per videogiochi(yeee)

--- src/main/java/generation/italy/org/ravenclaw/models/services/VideogiocoService.java
+++ src/main/java/generation/italy/org/ravenclaw/models/services/VideogiocoService.java
@@ -1,6 +1,7 @@
 package generation.italy.org.ravenclaw.models.services;
 
 import generation.italy.org.ravenclaw.models.entities.Videogioco;
+import generation.italy.org.ravenclaw.models.searchCriteria.VideogiocoFilterCriteria;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +12,6 @@ public interface VideogiocoService {
     boolean deleteById(int id);
     Videogioco save(Videogioco videogioco, int casaDiPubblicazione, int casaDiProduzione);
     Videogioco updateVideogioco(Videogioco videogioco, int casaDiProduzioneId, int casaDiPubblicazioneId);
+
+    List<Videogioco> searchProducts(VideogiocoFilterCriteria filters);
 }
