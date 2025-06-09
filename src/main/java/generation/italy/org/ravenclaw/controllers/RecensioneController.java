b4d6640d9e6fabba486c585c0727b953585ec363
LorenzoCoretti1 <lorenzo.coretti1@gmail.com>
2025-06-09 17:53:44 +0200

    Completa il controller delle recensioni

--- src/main/java/generation/italy/org/ravenclaw/controllers/RecensioneController.java
+++ src/main/java/generation/italy/org/ravenclaw/controllers/RecensioneController.java
@@ -1,10 +1,18 @@
 package generation.italy.org.ravenclaw.controllers;
 
+import generation.italy.org.ravenclaw.models.dtos.RecensioneDto;
+import generation.italy.org.ravenclaw.models.dtos.TagDto;
+import generation.italy.org.ravenclaw.models.entities.Recensione;
+import generation.italy.org.ravenclaw.models.entities.Tag;
 import generation.italy.org.ravenclaw.models.services.RecensioneService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -16,6 +24,62 @@ public class RecensioneController {
     public RecensioneController(RecensioneService recensioneService){
         this.recensioneService = recensioneService;
     }
+    @GetMapping
+    public ResponseEntity<List<RecensioneDto>> searchRecensione() {
+        List<Recensione> recensioneList = recensioneService.findAllRecensioni();
+        if (recensioneList.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        List<RecensioneDto> recensioneDto = recensioneList.stream().map(RecensioneDto::toDto).toList();
+        return ResponseEntity.ok(recensioneDto);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> searchById(@PathVariable int id){
+        Optional<Recensione> recensioneOptional = recensioneService.findRecensioneById(id);
+        if(recensioneOptional.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(RecensioneDto.toDto(recensioneOptional.get()));
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable int id){
+        Optional<Recensione> recensioneOptional = recensioneService.findRecensioneById(id);
+        if(recensioneOptional.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        boolean deleted = recensioneService.deleteRecensione(id);
+        if(!deleted){
+            ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createRecensione (@RequestBody RecensioneDto recensioneDto){
+        Recensione recensione = recensioneDto.toRecensione();
+        Recensione savedRecensione = recensioneService.saveRecensione(recensione);
+        RecensioneDto newRecensioneDto = RecensioneDto.toDto(savedRecensione);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newRecensioneDto.getRecensioneId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(newRecensioneDto);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateRecensione (@PathVariable int id, @RequestBody RecensioneDto recensioneDto){
+        Optional<Recensione> recensioneOptional = recensioneService.findRecensioneById(id);
+        if(id != recensioneDto.getRecensioneId()){
+            return ResponseEntity.badRequest().body("id dto e id del percorso non coincidono");
+        }
+        if(recensioneOptional.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        Recensione recensione = recensioneService.updateRecensione(recensioneOptional.get());
+        return ResponseEntity.ok(RecensioneDto.toDto(recensione));
+    }
 }
