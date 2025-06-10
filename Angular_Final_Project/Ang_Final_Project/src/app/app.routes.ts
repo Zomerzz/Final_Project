388e025099c505b0a00b733442ef29045375d69b
CamillaMar <camillamarchioro@fadbrera.edu.it>
2025-06-10 12:49:12 +0000

    aggiunge il module di http client

--- Angular_Final_Project/Ang_Final_Project/src/app/app.routes.ts
+++ Angular_Final_Project/Ang_Final_Project/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UserPageComponent } from './components/user-page-component/user-page-component';
+import { RecensioniListComponent } from './components/recensioni-list/recensioni-list.component';
 
 export const routes: Routes = [{ path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'user', component: UserPageComponent}];
+  { path: 'user', component: UserPageComponent},
+  { path: 'recensioni-list/:type/:id', component: RecensioniListComponent}];
