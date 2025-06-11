b0fe0316145699c25ab5fc069463e817d58cc22e
CamillaMar <camillamarchioro@fadbrera.edu.it>
2025-06-11 21:39:23 +0000

    toglie le prove dei component di recensione dalla home

--- Angular_Final_Project/Ang_Final_Project/src/app/components/home/home.component.ts
+++ Angular_Final_Project/Ang_Final_Project/src/app/components/home/home.component.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [RecensioniListComponent, AddRecensioneComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
