fca36b59dd3b7e0fb2d7eb362689b103b4d5b6da
Zomerzz <aresufilippo2005@gmail.com>
2025-06-12 09:29:45 +0200

    logica di auth

--- src/main/java/generation/italy/org/ravenclaw/controllers/AuthenticationController.java
+++ src/main/java/generation/italy/org/ravenclaw/controllers/AuthenticationController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/auth")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
