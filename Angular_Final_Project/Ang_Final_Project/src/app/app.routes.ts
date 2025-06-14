50bc34efdf6807a90d9605dde7c943097831e799
Colum-nar <edoardobognanni@gmail.com>
2025-06-14 16:38:01 +0100

    Risolve problemi di import in routes e bookdetails

--- Angular_Final_Project/Ang_Final_Project/src/app/app.routes.ts
+++ Angular_Final_Project/Ang_Final_Project/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './components/home/home.component';
 import { UserPageComponent } from './components/user-page-component/user-page-component';
 import { LoginAreaComponent } from './components/login-area-component/login-area-component';
 import { RegisterAreaComponent } from './components/register-area/register-area.component';
-import { DetailsComponent } from './components/details-component/details-component';
 import { AdminComponent } from './components/admin/admin.component';
 import { LogOutComponent } from './components/log-out/log-out.component';
 import { FilmDetailsComponent } from './components/details-component/film-details/film-details.component';
@@ -13,7 +12,6 @@ import { BookDetailsComponent } from './components/details-component/book-detail
 export const routes: Routes = [{ path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'user', component: UserPageComponent},
-  { path: 'game-detail/:id', component: DetailsComponent},
   { path: 'auth/login', component:LoginAreaComponent},
   { path: 'auth/logout', component:LogOutComponent},
   { path: 'auth/register', component:RegisterAreaComponent},
