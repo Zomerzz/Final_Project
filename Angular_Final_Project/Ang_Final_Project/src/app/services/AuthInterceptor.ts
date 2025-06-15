e074e039b3f8076dc51c5415413ec0462b09e416
CamillaMar <camillamarchioro@fadbrera.edu.it>
2025-06-15 10:07:58 +0000

    corregge un typ in AuthInterceptor.ts

--- Angular_Final_Project/Ang_Final_Project/src/app/services/AuthInterceptor.ts
+++ Angular_Final_Project/Ang_Final_Project/src/app/services/AuthInterceptor.ts
@@ -5,29 +5,28 @@ import { Router } from "@angular/router";
 import { catchError, throwError } from "rxjs";
 
 export const authInterceptorFn: HttpInterceptorFn = (req, next) =>{
-    const authService = inject(AuthService);
-    const token = authService.getToken();
-    const router = inject(Router);
+ const authService = inject(AuthService);
+  const token = authService.getToken();
+  const router = inject(Router);modifica
 
-    if(req.url.includes('api/auth/login') || req.url.includes('/api/auth/register')) {
-        return next(req);
-    }
+  if (req.url.includes('/api/auth/login') || req.url.includes('/api/auth/register')) {
+    return next(req);
+  }
 
-    const authReq = token ? req.clone({
-        setHeaders: {
-            Authorization: 'Bearer &{token}'
-        }
+  const authReq = token ? req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
     }) : req;
 
     return next(authReq).pipe(
-        catchError((error: HttpErrorResponse) => {
-            if(error.status == 401 || error.status == 403){
-                authService.logout();
-                router.navigate(['/auth/login']);
-                return throwError(() => "Errore nell'autenticazione");
-            }
-            return throwError(() => error);
+      catchError((error: HttpErrorResponse) => {
+        if(error.status === 401 || error.status === 403){
+          authService.logout();
+          router.navigate(['/login']);
+          return throwError(() => "errore nell'autenticazione. Ruolo invalido o sessione scaduta")
         }
-        )
+        return throwError(() => error);
+      })
     );
 }
