f853fb546728607a69bc624021a931ae64ffb2a0
CamillaMar <camillamarchioro@fadbrera.edu.it>
2025-06-17 18:09:19 +0000

    aggiunge getUtentiByUsername nel service di utente del front

--- Angular_Final_Project/Ang_Final_Project/src/app/services/UtenteService.ts
+++ Angular_Final_Project/Ang_Final_Project/src/app/services/UtenteService.ts
@@ -1,8 +1,16 @@
+import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Utente } from "../model/Utente";
 
 @Injectable({
     providedIn: 'root'
 })
 export class UtenteService {
+    private _url: string = 'http://localhost:8080/api/utente';
+    private _http: HttpClient = inject(HttpClient);
     
+    getUtentiByUsername(username: string):Observable<Utente[]>{
+        return this._http.get<Utente[]>(`${this._url}/username/${username}`);
+    }
 }
